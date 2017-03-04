a90bd846af9437b28fd693806c649597bf0c0431
Aditya Chandel <aditya.chandel@aggiemail.usu.edu>
2017-03-04 03:28:01 -0700

    Added HW1 and HW2 for CS6676 Advanced Data Mining

--- CS6676 - Advanced Data Mining/HW-2/Yelp/main.py
+++ CS6676 - Advanced Data Mining/HW-2/Yelp/main.py
@@ -1,4 +1,5 @@
 import json
+import csv
 from collections import OrderedDict
 
 
@@ -13,30 +14,75 @@ def load_checkins_to_dictionary():
     return c_ins
 
 
+def review_count(c_ins1):
+    count_business_reviews = {}
+    with open('D://Yelp/yelp_academic_dataset_review.json') as f3:
+        for line in f3:
+            business_id = json.loads(line)['business_id']
+            if business_id in c_ins1:
+                if business_id in count_business_reviews:
+                    count_business_reviews[business_id] += 1
+                else:
+                    count_business_reviews[business_id] = 1
+    return count_business_reviews
+
+
 def business_with_checkin_count(c_ins1):
     business_cins = {}
     with open('D://Yelp/yelp_academic_dataset_business.json') as f2:
         for line in f2:
-            if json.loads(line)['categories'] and 'Restaurants' in json.loads(line)['categories']:
+            if json.loads(line)['categories'] \
+                    and 'Restaurants' in json.loads(line)['categories'] \
+                    and (json.loads(line)['city'] == 'Charlotte'):
                 if json.loads(line)['business_id'] in c_ins1:
                     business_cins[json.loads(line)['business_id']] = [json.loads(line)['categories'], c_ins1[json.loads(line)['business_id']]]
     return business_cins
 
 
-if __name__ == "__main__":
-    checkins = load_checkins_to_dictionary()
-    data = business_with_checkin_count(checkins)
-
-    restaurant_type_count = {}
+def calculate_popular_food_types(data, cnt_reviews):
+    popular_food_types = {}
     for key, d in data.iteritems():
         for rest_type in d[0]:
             if rest_type != 'Restaurants':
-                if rest_type in restaurant_type_count:
-                    restaurant_type_count[rest_type] = restaurant_type_count[rest_type] + d[1]
+                if rest_type in popular_food_types:
+                    popular_food_types[rest_type] = popular_food_types[rest_type] + d[1] + cnt_reviews[key]
                 else:
-                    restaurant_type_count[rest_type] = d[1]
+                    popular_food_types[rest_type] = d[1] + cnt_reviews[key]
+    s = OrderedDict(sorted(popular_food_types.items(), key=lambda t: t[1], reverse=True))
+    for r in s:
+        print str(r) + " : " + str(popular_food_types[r])
+    return s
 
-    sorted = OrderedDict(sorted(restaurant_type_count.items(), key=lambda t: t[1]))
 
-    for r in sorted:
-        print str(r) + " : " + str(restaurant_type_count[r])
+def export_top10_food_location():
+    charlotte_top = ['Mexican', 'Southern', 'Italian', 'Japanese', 'Chinese', 'Vietnamese', 'Latin American', 'Thai', 'Greek', 'French']
+    pittsburg_top = ['Italian', 'Mexican', 'Chinese', 'Thai', 'Japanese', 'Mediterranean', 'Latin American', 'French', 'Middle Eastern', 'Indian']
+
+    with open('D://Yelp/yelp_academic_dataset_business.json') as f5:
+        business = []
+        for line in f5:
+            if json.loads(line)['city'] == 'Pittsburgh' and (4.5 <= json.loads(line)['stars'] <= 5.0):
+                for cat in json.loads(line)['categories']:
+                    if cat in pittsburg_top:
+                        business.append([
+                            json.loads(line)['name'].encode('ascii', 'ignore').decode('ascii'),
+                            json.loads(line)['city'],
+                            json.loads(line)['latitude'],
+                            json.loads(line)['longitude'],
+                            json.loads(line)['stars'],
+                            cat
+                        ])
+    with open("output.csv", "wb") as f:
+        writer = csv.writer(f)
+        writer.writerows(business)
+
+
+
+if __name__ == "__main__":
+    # checkins = load_checkins_to_dictionary()
+    # count_reviews = review_count(checkins)
+    # business_with_checkins_foods = business_with_checkin_count(checkins)
+    #
+    # pop_food = calculate_popular_food_types(business_with_checkins_foods, count_reviews)
+
+    export_top10_food_location()
